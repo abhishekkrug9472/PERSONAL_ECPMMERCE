ad908f217d5ececc0e99684c0da1ef64c2e40129
abhishekkr.ug21.ec@nitp.ac.in <121070042+abhishekkrug9472@users.noreply.github.com>
2024-07-16 15:52:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
@@ -20,20 +20,26 @@ dotenv.config();
 connectDB();
 //rest object
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// Handle any other requests by sending back the index.html file
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname,'./client/build')));
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
-})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
