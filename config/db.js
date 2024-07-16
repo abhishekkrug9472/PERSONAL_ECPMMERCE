a8d50cef40313b20350cf76b611c5738bc9af29b
abhishekkr.ug21.ec@nitp.ac.in <121070042+abhishekkrug9472@users.noreply.github.com>
2024-07-16 16:17:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://socialmedia:amit7488@cluster0.9rn4anf.mongodb.net/socialmedia");
+    const conn = await mongoose.connect("mongodb+srv://abhishek:abhi9472@cluster0.cckyvb6.mongodb.net/");
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
